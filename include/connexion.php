1afba986cc678fc1bbf5699dc024b297b5564a03
Jonathanranaivoson <jontahanranaivoson8@gmail.com>
2025-07-14 16:06:05 +0300

    adding comment on connexion for deployement

--- include/connexion.php
+++ include/connexion.php
@@ -4,7 +4,7 @@
     static $connect = null;
     
     if ($connect === null){
-        $connect = mysqli_connect('localhost', 'root', '', 'basetest');
+        $connect = mysqli_connect('localhost', 'root', '', 'basetest'); //'localhost', 'ETU004392', 'KyenYJ6D', 'db_s2_ETU004392'
 
           if (!$connect){
               die('Erreur de connexion a la base de donnees: ' . mysqli_connect_error());
